8cfcd71d7b0bc7c2f69ed23e64c8e03f8b93547f
nukusaba <velixi.vel@gmail.com>
2025-08-06 11:54:05 +0200

    fixed so that the theme will be reinstalled if its already installed

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-sass ./src/main.scss build/real-ann.css
+mkdir -p ./build
+sass ./src/main.scss ./build/real-ann.css
 echo "✔ Compiled"
-cp -r ./src/assets/ build/
-sudo cp -r ./build /usr/local/share/urn/themes/real-ann
+cp -r ./src/assets/ ./build/
+sudo bash -c "rm -rf /usr/local/share/urn/themes/real-ann && cp -r ./build /usr/local/share/urn/themes/real-ann" # avoid running two sudo commands
 echo "✔ Installed"
-rm -r build/*
+rm -r ./build/*
 
